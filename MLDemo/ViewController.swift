8db248ba93d2154d434d1f8c10d156acee3bbfae
丁暐哲 <peter779701@gmail.com>
2018-05-20 15:37:14 +0800

    Initial Commit

---
+++ MLDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLDemo
+//
+//  Created by PeterDing on 2018/5/20.
+//  Copyright © 2018年 DinDin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
